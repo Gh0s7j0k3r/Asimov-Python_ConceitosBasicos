a32dbebf5a2fd18dcaa5a2e78b111bb4b7a79c75
Gh0s7j0k3r <miguel.moises@ymail.com>
2023-09-24 23:46:19 -0300

    Desafio Acerte o Numero

---
+++ Desafio-AcerteoNumero.py
@@ -0,0 +1,7 @@
+'''Desafio - Crie um programa que:
+Escolhe um numero secreto.
+Pede por um chute do usuário.
+Indica se o usuário acertou ou não.
+Se não acerto, dá uma dica, dizendo se o numero é mais alto ou mais baixo.
+Repete isso até 3 vezes!'''
+
